0e4f61abc7ed2983c053117bf18725d3d33ece7d
Ameya-bit <ameyapanchal011@gmail.com>
2026-06-23 18:26:32 -0400

    expand the nietzche dataset and retrain

--- grab_nietzche.py
+++ grab_nietzche.py
@@ -5,6 +5,15 @@ urls = {
     "genealogy_of_morals":   "https://www.gutenberg.org/cache/epub/52319/pg52319.txt",
     "zarathustra":           "https://www.gutenberg.org/cache/epub/1998/pg1998.txt",
     "beyond_good_and_evil":  "https://www.gutenberg.org/cache/epub/4363/pg4363.txt",
+    "joyful_wisdom": "https://www.gutenberg.org/cache/epub/52881/pg52881.txt",
+    "human_all_too_human_1": "https://www.gutenberg.org/cache/epub/51935/pg51935.txt",
+    "human_all_too_human_2": "https://www.gutenberg.org/cache/epub/37841/pg37841.txt",
+    "dawn_of_day":        "https://www.gutenberg.org/cache/epub/39955/pg39955.txt",  # Daybreak
+    "ecce_homo":          "https://www.gutenberg.org/cache/epub/52190/pg52190.txt",
+    "birth_of_tragedy":   "https://www.gutenberg.org/cache/epub/51356/pg51356.txt",
+    "will_to_power_1_2":  "https://www.gutenberg.org/cache/epub/52914/pg52914.txt",
+    "will_to_power_3_4":  "https://www.gutenberg.org/cache/epub/52915/pg52915.txt",
+    "case_of_wagner":     "https://www.gutenberg.org/cache/epub/25012/pg25012.txt",
 }
 
 def strip_pg_boilerplate(text):
